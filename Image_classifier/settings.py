570ea50c61c231555871168211af8675a9ea11d2
llSiddharthll <siddharthgreat443@gmail.com>
2023-11-14 02:43:18 +0530

    done

--- Image_classifier/settings.py
+++ Image_classifier/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5+l%x=ot#o*=t0vw21k#+lk*h%+y%5lrp^e7bspp+og-v&@x5f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", '.vercel.app']
 
 # Application definition
 
