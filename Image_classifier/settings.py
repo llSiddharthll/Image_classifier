abe54473422618cb5e4561b5823435aeb30c4657
llSiddharthll <siddharthgreat443@gmail.com>
2023-11-14 02:56:35 +0530

    db add

--- Image_classifier/settings.py
+++ Image_classifier/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'Image_classifier.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'GG5DAgC6dcc246DAB35Cff1Fb3eDcFfD',
+        'HOST': 'roundhouse.proxy.rlwy.net',  # Set to the appropriate host
+        'PORT': '12729',       # Set to the appropriate port
     }
 }
+
 
 
 # Password validation
